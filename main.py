acb4ee5190025c28394c182a2191a52f30f544a8
Renzo Mendoza <143251538+MendozaJRL@users.noreply.github.com>
2024-12-05 19:38:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ def main():
     st.title("Lumina Flora: Plant Growth Stage Detection Model")
     st.write("The plant growth stage detecion model is a YOLOv8 Model that is capable of detecting plant growth stages specifically, germination, growing, and harvesting.")
 
+    # Load your YOLOv8 model
+    model = YOLO("40 Epoch Plant Growth Stage YOLOv8 Model.pt")
+    
+    # File uploader
+    uploaded_file = st.file_uploader("Upload an Image", type=["jpg", "jpeg", "png"])
+    
     # Provide options for users to choose from
     option = st.selectbox("Select Plant Type:", ["Level 1: Olmetie Lettuce", "Level 2: Thurinus Lettuce"])
     
